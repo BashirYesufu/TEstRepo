485f613b88455992f37bba381ee5015846ab3909
bash <yesufubashir@gmail.com>
2022-06-02 23:25:23 +0100

    add onboarding files

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ApexColors {
+  static const black = Color(0xFF111827);
+  static const orange = Color(0xFFFFAB63);
+  static const white = Color(0xFFFFFFFF);
+  static const grey = Color(0xFFE5E7EB);
+}
--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -5,6 +5,6 @@ class OnBoarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
